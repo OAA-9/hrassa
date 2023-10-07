2b55da29e7db786abc717b31400bdde18475cfc6
xingye <xingye.xy@foxmail.com>
2023-10-07 13:39:28 +0800

    登录模块

--- src/permission.js
+++ src/permission.js
@@ -1,64 +1,34 @@
-import router from './router'
-import store from './store'
-import { Message } from 'element-ui'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from '@/utils/auth' // get token from cookie
-import getPageTitle from '@/utils/get-page-title'
+// 权限拦截在路由跳转 导航守卫
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
+import router from '@/router'
+import store from '@/store'
+import nProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
-const whiteList = ['/login'] // no redirect whitelist
-
-router.beforeEach(async (to, from, next) => {
-  // start progress bar
-  NProgress.start()
-
-  // set page title
-  document.title = getPageTitle(to.meta.title)
-
-  // determine whether the user has logged in
-  const hasToken = getToken()
-
-  if (hasToken) {
+// 定义白名单  所有不受权限控制的页面
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+// 开启进度条
+  nProgress.start()
+  if (store.getters.token) {
     if (to.path === '/login') {
-      // if is logged in, redirect to the home page
-      next({ path: '/' })
-      NProgress.done()
+      // 如果访问的是登录页
+      next('/')
     } else {
-      const hasGetUserInfo = store.getters.name
-      if (hasGetUserInfo) {
-        next()
-      } else {
-        try {
-          // get user info
-          await store.dispatch('user/getInfo')
-
-          next()
-        } catch (error) {
-          // remove token and go to login page to re-login
-          await store.dispatch('user/resetToken')
-          Message.error(error || 'Has Error')
-          next(`/login?redirect=${to.path}`)
-          NProgress.done()
-        }
-      }
+      next()
     }
   } else {
-    /* has no token*/
-
-    if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
+    // 没有token的情况下
+    if (whiteList.indexOf(to.path) > -1) {
       next()
     } else {
-      // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
+      next('/login')
     }
+    // 解决手动切换进度条时 进度条不关闭的问题
+    nProgress.done()
   }
 })
 
 router.afterEach(() => {
-  // finish progress bar
-  NProgress.done()
+  nProgress.done()
 })
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,97 +1,34 @@
-import { login, logout, getInfo } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { resetRouter } from '@/router'
-
-const getDefaultState = () => {
-  return {
-    token: getToken(),
-    name: '',
-    avatar: ''
-  }
+import { login } from '@/api/user'
+// 状态
+const state = {
+  token: getToken()
 }
-
-const state = getDefaultState()
-
 const mutations = {
-  RESET_STATE: (state) => {
-    Object.assign(state, getDefaultState())
+  // 设置token
+  setToken (state, token) {
+    state.token = token // 设置token  只是修改state的数据  123 =》 1234
+    // vuex变化 => 缓存数据
+    setToken(token) // vuex和 缓存数据的同步
   },
-  SET_TOKEN: (state, token) => {
-    state.token = token
-  },
-  SET_NAME: (state, name) => {
-    state.name = name
-  },
-  SET_AVATAR: (state, avatar) => {
-    state.avatar = avatar
+  // 删除缓存
+  removeToken (state) {
+    state.token = null // 删除vuex的token
+    removeToken() // 先清除 vuex  再清除缓存 vuex和 缓存数据的同步
   }
 }
 
 const actions = {
-  // user login
-  login({ commit }, userInfo) {
-    const { username, password } = userInfo
-    return new Promise((resolve, reject) => {
-      login({ username: username.trim(), password: password }).then(response => {
-        const { data } = response
-        commit('SET_TOKEN', data.token)
-        setToken(data.token)
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // get user info
-  getInfo({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
-        const { data } = response
-
-        if (!data) {
-          return reject('Verification failed, please Login again.')
-        }
-
-        const { name, avatar } = data
-
-        commit('SET_NAME', name)
-        commit('SET_AVATAR', avatar)
-        resolve(data)
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // user logout
-  logout({ commit, state }) {
-    return new Promise((resolve, reject) => {
-      logout(state.token).then(() => {
-        removeToken() // must remove  token  first
-        resetRouter()
-        commit('RESET_STATE')
-        resolve()
-      }).catch(error => {
-        reject(error)
-      })
-    })
-  },
-
-  // remove token
-  resetToken({ commit }) {
-    return new Promise(resolve => {
-      removeToken() // must remove  token  first
-      commit('RESET_STATE')
-      resolve()
-    })
+  async login (context, data) {
+    // 调用api接口
+    const result = await login(data)
+    context.commit('setToken', result)
   }
 }
-
 export default {
   namespaced: true,
   state,
   mutations,
   actions
-}
 
+}
